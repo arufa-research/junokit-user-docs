514776eda438997bac69dd1d69d0cad72dc4338d
udit-gulati <uditgulati6@gmail.com>
2022-07-11 17:59:52 +0530

    git ignore build and docusaurus dirs

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/manishkumar/Documents/junokit-user-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/manishkumar/Documents/junokit-user-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/manishkumar/Documents/junokit-user-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/manishkumar/Documents/junokit-user-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/manishkumar/Documents/junokit-user-docs/src/css/custom.css'),
-];
